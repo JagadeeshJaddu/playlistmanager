5f0b62a73412822a4a8e1e1f7f276d3ff3b3d5b0
JagadeeshJaddu <jaddujagadeesh5146@gmail.com>
2023-11-01 11:46:35 +0530

    Updated a dto

--- src/main/java/com/spotify/playlistmanager/dtos/FindSongsByArtistResponseDTO.java
+++ src/main/java/com/spotify/playlistmanager/dtos/FindSongsByArtistResponseDTO.java
@@ -1,10 +1,16 @@
 package com.spotify.playlistmanager.dtos;
 
+import java.util.List;
+
+import com.spotify.playlistmanager.models.Song;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class FindSongsByArtistResponseDTO {
-    
+    private List<Song> songs;
+    private String status;
+    private String message;
 }
